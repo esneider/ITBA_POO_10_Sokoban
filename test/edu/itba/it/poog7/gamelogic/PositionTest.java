312dfa2bcf152751a16dd5581f5e6da724c7cf5e
champo <jcivile@gmail.com>
2010-05-31 02:58:55 +0000

    Unit Test for Position.
    git-svn-id: http://poog7-itba.googlecode.com/svn/trunk@18 0c7d088e-fb16-ab3f-9874-883ec5c5deb4

---
+++ test/edu/itba/it/poog7/gamelogic/PositionTest.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package edu.itba.it.poog7.gamelogic;
+
+import junit.framework.TestCase;
+
+/**
+ * @author champo
+ *
+ */
+public class PositionTest extends TestCase {
+
+	/**
+	 * Test method for {@link edu.itba.it.poog7.gamelogic.Position#Position(int, int)}.
+	 */
+	public void testPosition() {
+		
+		new Position(0, 0);
+	}
+
+	/**
+	 * Test method for {@link edu.itba.it.poog7.gamelogic.Position#getX()}.
+	 */
+	public void testGetX() {
+		
+		int x = 4;
+		Position position = new Position(x, 0);
+		
+		assertEquals(x, position.getX());
+	}
+
+	/**
+	 * Test method for {@link edu.itba.it.poog7.gamelogic.Position#getY()}.
+	 */
+	public void testGetY() {
+		
+		int y = -34;
+		Position position = new Position(0, y);
+		
+		assertEquals(y, position.getY());
+	}
+
+	/**
+	 * Test method for {@link edu.itba.it.poog7.gamelogic.Position#getNeighbourPosition(edu.itba.it.poog7.gamelogic.Direction)}.
+	 */
+	public void testGetNeighbourPosition() {
+		
+		Position pos = new Position(0, 0);
+		
+		assertEquals(pos, pos.getNeighbourPosition(Direction.DOWN).getNeighbourPosition(Direction.UP));
+		
+		Position result;
+		
+		//TODO: This just doesnt feel right
+		result = pos.getNeighbourPosition(Direction.UP);
+		assertFalse(pos.getX() == result.getX());
+		assertEquals(pos.getY(), result.getY());
+		
+		result = pos.getNeighbourPosition(Direction.DOWN);
+		assertFalse(pos.getX() == result.getX());
+		assertEquals(pos.getY(), result.getY());
+		
+		result = pos.getNeighbourPosition(Direction.LEFT);
+		assertEquals(pos.getX(), result.getX());
+		assertFalse(pos.getY() == result.getY());
+		
+		result = pos.getNeighbourPosition(Direction.RIGHT);
+		assertEquals(pos.getX(), result.getX());
+		assertFalse(pos.getY() == result.getY());
+	}
+
+	/**
+	 * Test method for {@link edu.itba.it.poog7.gamelogic.Position#equals(edu.itba.it.poog7.gamelogic.Direction)}.
+	 */
+	public void testEquals() {
+		
+		Position first = new Position(0, 0);
+		assertTrue(first.equals(new Position(0, 0)));
+		assertFalse(first.equals(new Position(1, 1)));
+		assertFalse(first.equals(new Position(0, 1)));
+	}
+
+}
