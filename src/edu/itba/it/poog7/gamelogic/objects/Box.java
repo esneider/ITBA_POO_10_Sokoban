a369b916473e3c047b5e85da5cba4c7f33db37fb
eordano <eordano@gmail.com>
2010-05-31 03:03:26 +0000

    Finished canMove() in both Box and Chaboncitou
    git-svn-id: http://poog7-itba.googlecode.com/svn/trunk@19 0c7d088e-fb16-ab3f-9874-883ec5c5deb4

--- src/edu/itba/it/poog7/gamelogic/objects/Box.java
+++ src/edu/itba/it/poog7/gamelogic/objects/Box.java
@@ -1,11 +1,24 @@
 package edu.itba.it.poog7.gamelogic.objects;
 
+import edu.itba.it.poog7.gamelogic.Direction;
+import edu.itba.it.poog7.gamelogic.LevelState;
 import edu.itba.it.poog7.gamelogic.Position;
+import edu.itba.it.poog7.gamelogic.tiles.Tile;
 
 public abstract class Box extends LevelObject {
 
 	public Box(Position pos) {
 		super(pos);
 	}
-
+	
+	public boolean canMove(LevelState state, Direction dir){
+		if (!super.canMove(state, dir)){
+			return false;
+		}
+		Tile nextTile = state.getTile(pos.getNeighbourPosition(dir));
+		if(nextTile.getObject() != null){
+			return false;
+		}
+		return true;
+	}
 }
--- src/edu/itba/it/poog7/gamelogic/objects/Chaboncitou.java
+++ src/edu/itba/it/poog7/gamelogic/objects/Chaboncitou.java
@@ -1,11 +1,31 @@
 package edu.itba.it.poog7.gamelogic.objects;
 
+import edu.itba.it.poog7.gamelogic.Direction;
+import edu.itba.it.poog7.gamelogic.LevelState;
 import edu.itba.it.poog7.gamelogic.Position;
+import edu.itba.it.poog7.gamelogic.objects.Box;
+import edu.itba.it.poog7.gamelogic.tiles.Tile;
 
 public abstract class Chaboncitou extends LevelObject {
 
 	public Chaboncitou(Position pos) {
 		super(pos);
 	}
-
+	
+	@Override
+	public boolean canMove(LevelState state, Direction dir){
+		if (!super.canMove(state, dir)){
+			return false;
+		}
+		Tile nextTile = state.getTile(pos.getNeighbourPosition(dir));
+		if (nextTile.getObject() != null){
+			if (nextTile.getObject() instanceof Box){
+				return nextTile.getObject().canMove(state, dir);
+			}
+			else{
+				return false;
+			}
+		}
+		return true;
+	}
 }
