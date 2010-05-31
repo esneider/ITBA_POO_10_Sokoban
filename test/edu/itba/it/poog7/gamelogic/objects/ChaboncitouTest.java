5565aa0a3b261b75fc7d855742e47f634d00a96d
eordano <eordano@gmail.com>
2010-05-31 03:46:32 +0000

    Stubbed some classes for testing
    git-svn-id: http://poog7-itba.googlecode.com/svn/trunk@25 0c7d088e-fb16-ab3f-9874-883ec5c5deb4

--- test/edu/itba/it/poog7/gamelogic/objects/ChaboncitouTest.java
+++ test/edu/itba/it/poog7/gamelogic/objects/ChaboncitouTest.java
@@ -1,9 +1,16 @@
-package test.edu.itba.it.poog7.gamelogic.objects;
+package test.edu.itba.it.poog7.gamelogic.objects
 
+import javax.swing.JPanel;
+
+import edu.itba.it.poog7.gamelogic.LevelState;
+import edu.itba.it.poog7.gamelogic.Position;
+import edu.itba.it.poog7.gamelogic.TileMatrix;
+import edu.itba.it.poog7.gamelogic.tiles.Blank;
+import edu.itba.it.poog7.gamelogic.tiles.Tile;
+import edu.itba.it.poog7.gamelogic.tiles.Wall;
 import junit.framework.TestCase;
 
 public class ChaboncitouTest extends TestCase {
-
 	public ChaboncitouTest(String arg0) {
 		super(arg0);
 	}
@@ -16,15 +23,54 @@ public class ChaboncitouTest extends TestCase {
 		super.tearDown();
 	}
 
-	public final void testCanMove() {
+	public void testCanMove() {
 		fail("Not yet implemented"); // TODO
 	}
 
-	public final void testMove() {
+	public void testMove() {
 		fail("Not yet implemented"); // TODO
 	}
-	
-	class LevelStateStub extends LevelState{
+	class TileMatrixStub extends TileMatrix{
+		TileMatrixStub(){
+			this.matrix = new Tile[5][5];
+			// Make a simple room
+			for(int i = 0; i < 5; i++){
+				for(int j = 0; j < 5; j++){
+					matrix[i][j] = new WallStub(new Position(i, j));
+				}
+				if (i > 0 && i < 4){
+					for(int j = 1; j < 4; j++){
+						matrix[i][j] = new BlankStub(new Position(i, j));
+					}
+				}
+			}
+		}
+	}
+	class WallStub extends Wall{
+		public WallStub(Position pos) {
+			super(pos);
+		}
+
+		@Override
+		public void draw(JPanel panel) {
+			return;
+		}
 		
+	}
+	class BlankStub extends Blank{
+		public BlankStub(Position pos) {
+			super(pos);
+		}
+
+		@Override
+		public void draw(JPanel panel) {
+			return;
+		}
+		
+	}
+	class LevelStateStub extends LevelState{
+		LevelStateStub(){
+			matrix = new TileMatrixStub();
+		}
 	}
 }
