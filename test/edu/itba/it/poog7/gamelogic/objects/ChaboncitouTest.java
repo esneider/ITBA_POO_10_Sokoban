8516d6a15e7b5792176c203dba36e657d0949268
eordano <eordano@gmail.com>
2010-05-31 03:23:19 +0000

    Added stub unit test for Chaboncitou
    git-svn-id: http://poog7-itba.googlecode.com/svn/trunk@23 0c7d088e-fb16-ab3f-9874-883ec5c5deb4

---
+++ test/edu/itba/it/poog7/gamelogic/objects/ChaboncitouTest.java
@@ -0,0 +1,30 @@
+package test.edu.itba.it.poog7.gamelogic.objects;
+
+import junit.framework.TestCase;
+
+public class ChaboncitouTest extends TestCase {
+
+	public ChaboncitouTest(String arg0) {
+		super(arg0);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public final void testCanMove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testMove() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	class LevelStateStub extends LevelState{
+		
+	}
+}
