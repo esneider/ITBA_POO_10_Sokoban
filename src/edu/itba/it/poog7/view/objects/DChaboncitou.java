3c5eb7800139b6d2fb3f49f23d7466d32a8e4151
esneider <dariosn@gmail.com>
2010-06-03 17:43:04 +0000

    fixing
    git-svn-id: http://poog7-itba.googlecode.com/svn/trunk@63 0c7d088e-fb16-ab3f-9874-883ec5c5deb4

---
+++ src/edu/itba/it/poog7/view/objects/DChaboncitou.java
@@ -0,0 +1,27 @@
+package edu.itba.it.poog7.view.objects;
+
+import java.io.IOException;
+
+import edu.itba.it.poog7.gamelogic_.objects.Chaboncitou;
+import edu.itba.it.poog7.view.*;
+
+/**
+ * {@link DrawableElement} corresponding to {@link Chaboncitou}
+ * 
+ * @author dario
+ *
+ */
+public class DChaboncitou extends DrawableElement {
+
+	/**
+	 * @param board        the {@link Board} in which to draw
+	 * @param chaboncitou  the corresponding {@link Chaboncitou}
+	 * 
+	 * @throws IOException  if there is an error while opening the image file
+	 */
+	public DChaboncitou(Board board, Chaboncitou chaboncitou) throws IOException {
+		super(board,chaboncitou);
+		image.setImage("resources/smile.png");
+		image.setTransparent(true);
+	}
+}
---
+++ src/edu/itba/it/poog7/view/tiles/DOneWay.java
@@ -0,0 +1,37 @@
+package edu.itba.it.poog7.view.tiles;
+
+import java.io.IOException;
+
+import edu.itba.it.poog7.gamelogic_.tiles.OneWay;
+import edu.itba.it.poog7.view.*;
+
+/**
+ * {@link DrawableElement} corresponding to {@link OneWay}
+ * 
+ * @author dario
+ *
+ */
+public class DOneWay extends DrawableElement {
+
+	/**
+	 * @param board   the {@link Board} in which to draw
+	 * @param oneWay  the corresponding {@link OneWay}
+	 * 
+	 * @throws IOException if there is an error while opening the image file
+	 */
+	public DOneWay(Board board, OneWay oneWay) throws IOException {
+		super(board,oneWay);
+		image.setImage("resources/path.png");
+		switch (oneWay.getDirection()) {
+			case RIGHT:
+				image.rotate(1);
+				break;
+			case DOWN:
+				image.rotate(2);
+				break;
+			case LEFT:
+				image.rotate(3);
+				break;
+		}
+	}
+}
