cf9646224311d17a4ddfdabacaa04101307353a9
eordano <eordano@gmail.com>
2010-06-03 19:21:34 +0000

    Reverted Box and Chaboncitou, marged with r55 changes
    git-svn-id: http://poog7-itba.googlecode.com/svn/trunk@71 0c7d088e-fb16-ab3f-9874-883ec5c5deb4

--- src/edu/itba/it/poog7/gamelogic/objects/Chaboncitou.java
+++ src/edu/itba/it/poog7/gamelogic/objects/Chaboncitou.java
@@ -3,20 +3,32 @@ package edu.itba.it.poog7.gamelogic.objects;
 import edu.itba.it.poog7.gamelogic.Direction;
 import edu.itba.it.poog7.gamelogic.Game;
 import edu.itba.it.poog7.gamelogic.Position;
+import edu.itba.it.poog7.gamelogic.objects.Box;
 import edu.itba.it.poog7.gamelogic.tiles.Tile;
-
 
 /**
  * 
  * @author eordano
  *
  */
-public abstract class Chaboncitou extends GameObject {
+public class Chaboncitou extends GameObject {
 
+	/**
+	 * Simple constructor that delegates everything to his father.
+	 * 
+	 * @param pos
+	 */
 	public Chaboncitou(Position pos) {
 		super(pos);
 	}
 	
+	/**
+	 * Check for the ability to move into another square. If the square is occupied by a box, there
+	 * is still a chance that he can move it.
+	 * 
+	 * @param state    The current instance of Game where the Chaboncitou is in.
+	 * @param dir      The direction in which to move.
+	 */
 	public boolean canMove(Game state, Direction dir){
 		if (!super.canMove(state, dir)){
 			return false;
@@ -33,9 +45,13 @@ public abstract class Chaboncitou extends GameObject {
 		return true;
 	}
 	
+	/**
+	 * Refactored into a dark deprecated world?
+	 * 
+	 * This went away in the BROTT
+	 */
 	@Override
 	public void destructor(Game state) {
-
 		super.destructor(state);
 		state.setChaboncitouPos(null);
 	}
