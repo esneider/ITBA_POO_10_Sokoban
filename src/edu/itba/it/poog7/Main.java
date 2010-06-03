30490979a4836d57c1428d01a57123091fa624c3
esneider <dariosn@gmail.com>
2010-06-03 17:25:11 +0000

    Main (just to see if everything makes sense)
    git-svn-id: http://poog7-itba.googlecode.com/svn/trunk@60 0c7d088e-fb16-ab3f-9874-883ec5c5deb4

---
+++ src/edu/itba/it/poog7/Main.java
@@ -0,0 +1,15 @@
+package edu.itba.it.poog7;
+
+import edu.itba.it.poog7.controller_.Controller;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Controller controller = new Controller();
+	}
+
+}
