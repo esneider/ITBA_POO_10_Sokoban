688ce31ccc2b8ef25355b47e150d2790ebccabf4
champo <jcivile@gmail.com>
2010-06-05 14:50:30 +0000

    Yet another missing import.
    git-svn-id: http://poog7-itba.googlecode.com/svn/trunk@77 0c7d088e-fb16-ab3f-9874-883ec5c5deb4

--- src/edu/itba/it/poog7/view/tiles/DTarget.java
+++ src/edu/itba/it/poog7/view/tiles/DTarget.java
@@ -2,6 +2,7 @@ package edu.itba.it.poog7.view.tiles;
 
 import java.io.IOException;
 
+import edu.itba.it.poog7.gamelogic.tiles.Target;
 import edu.itba.it.poog7.view.Board;
 import edu.itba.it.poog7.view.DrawableElement;
 
@@ -20,7 +21,7 @@ public class DTarget extends DrawableElement {
 	 * @throws IOException if there is an error while opening the image file
 	 */
 	public DTarget(Board board, Target target) throws IOException {
-		super(board,target);
+		super(board, target);
 		image.setImage("resources/target.png");
 		image.dye(target.getColor());
 	}
