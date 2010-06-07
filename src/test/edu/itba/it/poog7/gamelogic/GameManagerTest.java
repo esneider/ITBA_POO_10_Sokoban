4e6c9f1a2e880b6a1da665412d1dff1a987f6ac2
eordano <eordano@gmail.com>
2010-06-07 03:31:37 +0000

    Tests from the GIT Repository
    git-svn-id: http://poog7-itba.googlecode.com/svn/trunk@80 0c7d088e-fb16-ab3f-9874-883ec5c5deb4

---
+++ src/test/edu/itba/it/poog7/gamelogic/GameManagerTest.java
@@ -0,0 +1,27 @@
+package test.edu.itba.it.poog7.gamelogic;
+
+import junit.framework.TestCase;
+
+public class GameManagerTest extends TestCase {
+
+	public final void testGetLevelList() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetNextLevel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testLoadGame() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSaveGame() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testLoadLevel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
