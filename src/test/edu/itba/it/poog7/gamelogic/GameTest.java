a3defb676cbae2840ce6a1c433a8e841d517788c
eordano <eordano@gmail.com>
2010-06-08 01:20:30 +0000

    More on GameTest and funny comment
    git-svn-id: http://poog7-itba.googlecode.com/svn/trunk@99 0c7d088e-fb16-ab3f-9874-883ec5c5deb4

--- src/test/edu/itba/it/poog7/gamelogic/GameTest.java
+++ src/test/edu/itba/it/poog7/gamelogic/GameTest.java
@@ -1,10 +1,13 @@
 package test.edu.itba.it.poog7.gamelogic;
 
 import junit.framework.TestCase;
+import edu.itba.it.poog7.event.Event;
+import edu.itba.it.poog7.event.EventListener;
 import edu.itba.it.poog7.gamelogic.Direction;
 import edu.itba.it.poog7.gamelogic.Game;
 import edu.itba.it.poog7.gamelogic.Position;
 import edu.itba.it.poog7.gamelogic.RGBColor;
+import edu.itba.it.poog7.gamelogic.event.GameFinishedEvent;
 import edu.itba.it.poog7.gamelogic.objects.Box;
 import edu.itba.it.poog7.gamelogic.objects.Character;
 import edu.itba.it.poog7.gamelogic.objects.event.MoveCharacterEvent;
@@ -25,7 +28,9 @@ public class GameTest extends TestCase {
 		tiles[0][0] = new Blank(new Position(0, 0));
 		tiles[0][1] = new Blank(new Position(0, 1));
 		tiles[0][2] = new Target(new Position(0, 2), new RGBColor(0, 0, 0));
-		aGame.init("1", "2", "3", tiles, 0, 1, 4);
+		aGame.init("1", "2", "3", tiles, 0, 0, 1); // 0 score, 1 boxes
+		// (different testcases
+		// use one box, 1 target
 	}
 
 	public final void testInit() {
@@ -36,6 +41,7 @@ public class GameTest extends TestCase {
 	}
 
 	public final void testMoveCharacter() {
+
 		character.getMoveListener().eventTriggered(
 				new MoveCharacterEvent(aGame, Direction.DOWN));
 		assertTrue(aGame.getTile(new Position(0, 1)).getObject().equals(
@@ -48,20 +54,84 @@ public class GameTest extends TestCase {
 				new MoveCharacterEvent(aGame, Direction.UP));
 		assertTrue(aGame.getTile(new Position(0, 1)).getObject().equals(
 				character));
+
+		assertTrue(aGame.getNumMoves() == 3);
 	}
 
 	public final void testMoveCharacterAndBox() {
-		
+
 		// Set up a Box under the Character
 		Box blackBox = new Box(new Position(0, 1), new RGBColor(0, 0, 0));
 		aGame.getTile(new Position(0, 1)).setObject(blackBox);
-		
+
 		character.getMoveListener().eventTriggered(
 				new MoveCharacterEvent(aGame, Direction.DOWN));
 		assertTrue(aGame.getTile(new Position(0, 1)).getObject().equals(
 				character));
 		assertTrue(aGame.getTile(new Position(0, 2)).getObject().equals(
 				blackBox));
+
 	}
 
+	public final void testWonIfSameColorBox() {
+
+		// Set up a Box under the Character
+		Box blackBox = new Box(new Position(0, 1), new RGBColor(0, 0, 0));
+		aGame.getTile(new Position(0, 1)).setObject(blackBox);
+
+		int who[];
+		who = new int[] { 0 };
+		StubCheck e = new StubCheck(who);
+		aGame.subscribeListener(GameFinishedEvent.class, e);
+
+		character.getMoveListener().eventTriggered(
+				new MoveCharacterEvent(aGame, Direction.DOWN));
+		assertTrue(aGame.getTile(new Position(0, 1)).getObject().equals(
+				character));
+		assertTrue(aGame.getTile(new Position(0, 2)).getObject().equals(
+				blackBox));
+
+		assertTrue(who[0] == 1);
+	}
+
+	/**
+	 * 
+	 */
+	public final void testNotWonIfNotTheSameColorBox() {
+
+		// Set up a Box under the Character
+		Box notSoBlackBox = new Box(new Position(0, 1), new RGBColor(0, 255, 0));
+		aGame.getTile(new Position(0, 1)).setObject(notSoBlackBox);
+
+		int who[];
+		who = new int[] { 0 };
+		StubCheck e = new StubCheck(who);
+		aGame.subscribeListener(GameFinishedEvent.class, e);
+
+		character.getMoveListener().eventTriggered(
+				new MoveCharacterEvent(aGame, Direction.DOWN));
+		assertTrue(aGame.getTile(new Position(0, 1)).getObject().equals(
+				character));
+		assertTrue(aGame.getTile(new Position(0, 2)).getObject().equals(
+				notSoBlackBox));
+
+		assertTrue(who[0] == 0);
+	}
+
+	/**
+	 * "En la guerra y en los unit tests, todo se vale" - Julius Caesar et al
+	 * 
+	 * This class uses a dirty trick to pass a C-like "output parameter"
+	 */
+	class StubCheck implements EventListener {
+		int[] who;
+
+		public void eventTriggered(Event e) {
+			who[0] = 1;
+		}
+
+		public StubCheck(int[] who) {
+			this.who = who;
+		}
+	}
 }
